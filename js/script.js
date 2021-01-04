161d4640f03576c613fce92a7da3f76a01ea5b4f
Anderson Vasconcelos <ciclista-rs@hotmail.com>
2021-01-04 19:55:21 -0300

    tabs

--- js/script.js
+++ js/script.js
@@ -1,17 +1,32 @@
 const map = [
-    "TTTTTTA",
-    "TTTTTTV",
     "TTTTTTT",
     "TTTTTTT",
     "TTTTTTT",
     "TTTTTTT",
+    "TTTTTTT",
+    "TTAVTTT",
 ];
+
+
+//
+
+
+
+
 
 // cria parede caminho, entrada e saida com divs
 for (let i = 0; i < map.length; i++) {
+
+
+    ///
+
+
     for (let a = 0; a < map[i].length; a++) {
         var torre = document.createElement('div');
         torre.setAttribute("id", map[i][a]);
         document.getElementById('lab').appendChild(torre);
     }
+
+
+
 }
