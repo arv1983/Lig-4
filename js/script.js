828536c154b6fb0e358f4c07b1021986cb49342a
Anderson Vasconcelos <ciclista-rs@hotmail.com>
2021-01-05 17:48:37 -0300

    funcao ganhador funcionando

--- js/script.js
+++ js/script.js
@@ -1,18 +1,10 @@
-// let map = [
-//     "TTTTTTT",
-//     "TTTTTTA",
-//     "TTTTTAT",
-//     "TTTTATT",
-//     "TTTATTT",
-//     "TTTTAAA",
-// ];
 let map = [
     ['T', 'T', 'T', 'T', 'T', 'T', 'T'],
     ['T', 'T', 'T', 'T', 'T', 'T', 'T'],
     ['T', 'T', 'T', 'T', 'T', 'T', 'T'],
     ['T', 'T', 'T', 'T', 'T', 'T', 'T'],
-    ['T', 'T', 'T', 'T', 'T', 'A', 'T'],
-    ['T', 'T', 'T', 'T', 'A', 'T', 'T']
+    ['T', 'T', 'T', 'T', 'T', 'T', 'T'],
+    ['T', 'T', 'T', 'T', 'T', 'T', 'T']
 ];
 
 
@@ -24,15 +16,15 @@ verifica_ganhador()
 function verifica_ganhador() {
     for (let i = 0; i < map.length; i++) {
         ////////////// verifica horizontal
-        if (map[i].indexOf('AAAA') > 0) {
+        if (map[i].join('').indexOf('AAAA') > 0) {
             console.log('azul ganha')
         }
-        if (map[i].indexOf('VVVV') > 0) {
+        if (map[i].join('').indexOf('VVVV') > 0) {
             console.log('vermelho ganha')
         }
 
 
-        //////////// verifica vertical
+        //////////// verifica vertical DIREITA
         let conta_a = 0;
         let conta_v = 0;
         for (let a = 0; a <= 5; a++) {
@@ -61,10 +53,15 @@ function verifica_ganhador() {
 
     }
 
-    //////////// verifica diagonal
+    //////////// verifica diagonal direita
     for (let a = 5; a > 0; a--) {
         for (let i = 0; i <= 6; i++) {
-            console.log(map[5][i])
+            if (map[a][i] == 'A' && map[a - 1][i - 1] == 'A' && map[a - 2][i - 2] == 'A' && map[a - 3][i - 3] == 'A') {
+                console.log('Ganhou Azul')
+            }
+            if (map[a][i] == 'V' && map[a - 1][i - 1] == 'V' && map[a - 2][i - 2] == 'V' && map[a - 3][i - 3] == 'V') {
+                console.log('Ganhou vermelho')
+            }
 
             if (map[a][i] == 'A' && map[a - 1][i + 1] == 'A' && map[a - 2][i + 2] == 'A' && map[a - 3][i + 3] == 'A') {
                 console.log('Ganhou Azul')
@@ -80,6 +77,22 @@ function verifica_ganhador() {
 
     }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
 
 
@@ -90,6 +103,8 @@ function verifica_ganhador() {
 // cria parede caminho, entrada e saida com divs
 
 cria_jogo(map);
+let par_ou_impar = 0;
+let jogador = 0;
 
 function cria_jogo() {
     for (let i = 0; i < map.length; i++) {
@@ -103,12 +118,43 @@ function cria_jogo() {
             torre.setAttribute("class", map[i][a]);
 
             document.getElementById('lab').appendChild(torre).onclick = function() {
+                par_ou_impar += 1;
+
+
+                if (par_ou_impar % 2 == 0) {
+                    jogador = 'A';
+                } else {
+                    jogador = 'V';
+                }
 
                 /// clicks nas colunas
                 /// map 
 
-                console.log('clicou ' + a + 'array' + map[5][a])
-                map[5][a] = 'A';
+
+
+
+
+                ////// funcao mmanipula array
+
+
+                for (let i = 0; i < 6; i++) {
+
+
+
+                    if (map[i][a] != 'T') {
+                        map[i - 1][a] = jogador;
+                        break;
+                    }
+                    if (i == 5) {
+                        map[i][a] = jogador;
+                    }
+
+
+                }
+
+
+
+
                 document.getElementById("lab").remove();
 
 
@@ -118,13 +164,12 @@ function cria_jogo() {
 
                 document.body.appendChild(divNova)
 
-
+                //recria
                 cria_jogo(map);
+                verifica_ganhador();
 
-                //
+
             };
-
         }
-
     }
 }
